5c213ff3bc17bd04e0e4cf5ebcfbee0a57d17a7d
WhoInGodsName <61045310+WhoInGodsName@users.noreply.github.com>
2024-05-01 20:48:06 +0800

    TeamManager class start

---
+++ RoRModNET4/TeamManager.cs
@@ -0,0 +1,31 @@
+﻿using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RoRModNET4
+{
+    internal class TeamManager : MonoBehaviour
+    {
+        public TeamManager()
+        {
+
+        }
+        private List<NetworkUser> GetTeam()
+        {
+            List<NetworkUser> result = new List<NetworkUser>();
+            foreach (NetworkUser netuser in NetworkUser.FindObjectsOfType(typeof(NetworkUser)))
+            {
+                /*if (netuser.masterController.master == LocalPlayer)
+                {
+                    continue;
+                }*/
+                result.Add(netuser);
+            }
+            return result;
+        }
+    }
+}
